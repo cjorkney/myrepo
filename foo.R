758a21ce63091fb2653bf8114bd607296a5fecf7
Callum Orkney <callum.orkney@gmail.com>
2020-12-06 17:52:45 +0000

    Make R from Rmd and produce markdown

---
+++ foo.R
@@ -0,0 +1,14 @@
+#'---
+#'title: "A test markdown"
+#'author: "Callum Orkney"
+#'date: "03/12/2020"
+#'output: github_document
+#'---
+
+#'Hello, this is a markdown document!
+
+#+ r code
+some_vector <- c(1, 2, 3)
+print(some_vector)
+
+
---
+++ foo.md
@@ -0,0 +1,13 @@
+A test markdown
+================
+Callum Orkney
+03/12/2020
+
+Hello, this is a markdown document\!
+
+``` r
+some_vector <- c(1, 2, 3)
+print(some_vector)
+```
+
+    ## [1] 1 2 3
